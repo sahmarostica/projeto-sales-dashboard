bf25ff5ed6fcada1fafb4b174022fa86c060667a
Sara Marostica <sah.marostica@gmail.com>
2021-05-05 18:17:34 +0100

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,32 @@
 import NavBar from 'components/NavBar';
 import Footer from 'components/footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
+
 
 function App() {
   return (
     <>
     <NavBar />
     <div className="container">
-      <h1 className="text-primary">Hello World</h1>
+      <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+      
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <BarChart />
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Total Vendas</h5>
+          <DonutChart />
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary"> Todas Vendas</h2>
+        </div>
+      </div>
+      
       <DataTable />
     </div>
     
