d51c5a0cf22fcdc391e3d1d4df107690d940c49b
Sara Marostica <sah.marostica@gmail.com>
2021-05-05 17:23:12 +0100

    Basic status component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/footer';
+import DataTable from 'components/DataTable';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello</h1>
+    <>
+    <NavBar />
+    <div className="container">
+      <h1 className="text-primary">Hello World</h1>
+      <DataTable />
     </div>
+    
+
+    <Footer />
+    </>
   );
 }
 
